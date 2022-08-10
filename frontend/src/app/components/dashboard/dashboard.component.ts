d3e9271696ababae17a68249ab2e6ccd129fd4ae
mafahla <tokamnaka97@gmail.com>
2022-08-11 01:24:23 +0200

    toka

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from 'src/app/shared/api.service';
 import { studentModel } from './student-module';
 
 
@@ -9,9 +10,13 @@ import { studentModel } from './student-module';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
 studentValue!:FormGroup;
 studentObj:studentModel = new studentModel;
-  constructor( private formBuilder: FormBuilder) { }
+studentList:any =[ ]
+btnSaveShow:boolean =true;
+btnUpdateShow:boolean=false;
+  constructor( private formBuilder: FormBuilder,private api: ApiService) { }
 
   ngOnInit(): void {
     this.studentValue= this.formBuilder.group({
@@ -20,17 +25,107 @@ studentObj:studentModel = new studentModel;
       email:[''],
       phone:['']
     });
+    this.getStudent();
   }
-
+  
   addStudent(){
-    // this.studentObj.name = this.formValue.value.name;
-    // this.studentObj.email = this.formValue.value.email;
-    // this.studentObj.phone = this.formValue.value.phone;
-    // this.studentObj.class = this.formValue.value.class;
-    //this.studentObj. = this.formValue.value.name;
+    
+    this.studentObj.name = this.studentValue.value.name;
+    this.studentObj.email = this.studentValue.value.email;
+     this.studentObj.phone = this.studentValue.value.phone;
+     this.studentObj.class = this.studentValue.value.class;
+
+     this.api.postStudent(this.studentObj).subscribe({next:(v)=>{
+      console.log(v)
+     },
+    error:(e)=>{
+      console.log(e)
+      alert("Error")
+    },
+  complete:()=>{
+    console.log('Student record added!')
+    alert("Student record added!")
+    this.getStudent();
+    this.studentValue.reset();
+  }})
     
   }
 
-  
+
+getStudent(){
+ this.api.getStudent(this).subscribe(res=>{ //error her 55.24  this.studentList = res;
+this.studentList = res; 
+}) 
+}
+
+deleteStudent(data:any){
+  this.api.deleteStudent(data.id).subscribe({next : (v)=>{
+    console.log(v)
+  },
+ error:(e)=>{
+   console.log(e)
+   alert("Error")
+ },
+complete:()=>{
+ console.log('Student record deleted!')
+ alert("Student record deleted!")
+ this.getStudent();
+}})
+}
+
+
+editStudent(data:any){
+this.studentValue.controls["name"].setValue(data.name);
+this.studentValue.controls["email"].setValue(data.email);
+this.studentValue.controls["phone"].setValue(data.phone);
+this.studentValue.controls["class"].setValue(data.class);
+this.studentObj.id = data.id;
+this.showUpdate()
+
+//this.api.editStudent(data.id).subscribe({next : (v)=>{
+//     console.log(v)
+//   },
+//  error:(e)=>{
+//    console.log(e)
+//    alert("Error")
+//  },
+// complete:()=>{
+//  console.log('Student record deleted!')
+//  alert("Student record deleted!")
+//  this.getStudent();
+// }})
+ }
+updateStudent(){
+  this.studentObj.name = this.studentValue.value.name;
+    this.studentObj.email = this.studentValue.value.email;
+     this.studentObj.phone = this.studentValue.value.phone;
+     this.studentObj.class = this.studentValue.value.class;
+
+     this.api.putStudent(this.studentObj, this.studentObj.id).subscribe({next:(v)=>{
+      console.log(v)
+     },
+    error:(e)=>{
+      console.log(e)
+      alert("Error")
+    },
+  complete:()=>{
+    console.log('Student record Updated!')
+    alert("Student record Updated!")
+    this.getStudent();
+    this.studentValue.reset();
+    this.showSave()
+    this.studentObj.id = 0;
+  }})
+
+}
+showSave(){
+this.btnSaveShow=true;
+this.btnUpdateShow=false;
+}
+showUpdate(){
+  this.btnSaveShow=false;
+  this.btnUpdateShow=true;
+  }
+
 
 }
--- frontend/src/app/shared/api.service.ts
+++ frontend/src/app/shared/api.service.ts
@@ -1,50 +1,51 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { map } from 'rxjs';
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ApiService {
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
 
-//   constructor(private http: HttpClient) { }
-// }
+  constructor(private http: HttpClient) { }
 
-// //post
 
-// postStudent(data:any){
-//   return this.http.post<any>("http://localhost:3000/post").pipe{
-//     map(
-//       (res => {
-//         return res
-//       })
-//     )
-//   }
-// }
-// getStudent(data:any){
-//   return this.http.get<any>("http://localhost:3000/post",data).pipe{
-//     map(
-//       (res => {
-//         return res
-//       })
-//     )
-//   }
-// }
-// putStudent(data:any,id:number){
-//   return this.http.put<any>("http://localhost:3000/post",id,data).pipe{
-//     map(
-//       (res => {
-//         return res
-//       })
-//     )
-//   }
-// }
-// deleteStudent(id:number){
-//   return this.http.delete<any>("http://localhost:3000/post",id).pipe{
-//     map(
-//       (res => {
-//         return res
-//       })
-//     )
-//   }
-// }
+//post
 
+postStudent(data:any){
+  return this.http.post<any>("http://localhost:3000/post",data).pipe(
+    map(
+      (res => {
+        return res
+      })
+    )
+  )
+}
+getStudent(data:any){
+  return this.http.get<any>("http://localhost:3000/post",data).pipe(
+    map(
+      (res => {
+        return res
+      })
+    )
+  )
+}
+putStudent(data:any,id:any){
+  return this.http.put<any>("http://localhost:3000/post"+id,data).pipe(
+    map(
+      (res => {
+        return res
+      })
+    )
+  )
+}
+deleteStudent(id:number){
+  return this.http.delete<any>("http://localhost:3000/post"+id).pipe(
+    map(
+      (res => {
+        return res
+      })
+    )
+  )
+}
+
+} 
